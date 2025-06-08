44f2cc8147008024ffb73b3e0ca95d2a0ba711d1
Wilmer Casilimas Sanchez <wilmercasilimas@gmail.com>
2025-06-07 20:57:47 -0400

    nada

--- routes/reacciones.js
+++ routes/reacciones.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const agregarReaccion = require("../controllers/reaccionesPublicacion");
+const agregarReaccion = require("../controllers/reacciones");
 const auth = require("../middlewares/auth");
 
 router.post("/agregar", auth, agregarReaccion);
